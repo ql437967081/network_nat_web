eda7b30e0aebe11cf799d2748da71512dd9112c0
161250106 <437967081@qq.com>
2020-12-18 19:14:56 +0800

    路由器连接、断开连接

--- src/page/router.js
+++ src/page/router.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button, Card } from 'antd';
+import { LogoutOutlined } from '@ant-design/icons';
 import { withRouter } from 'react-router-dom';
 
 class Router extends React.Component{
@@ -8,20 +10,40 @@ class Router extends React.Component{
         this.state = { router_id };
     }
 
+    logout = () => {
+        this.props.history.push('/home');
+    };
+
     componentDidMount() {
-        console.log('获取路由器信息。。。');
         const { router_id } = this.state;
+        console.log(`连接R${router_id}。。。`);
+        console.log('获取路由器信息。。。');
         console.log(`router_id: ${router_id}`);
+    }
+
+    componentWillUnmount() {
+        const { router_id } = this.state;
+        console.log(`断开连接R${router_id}。。。`);
     }
 
     render() {
         const { router_id } = this.state;
         return (
-            <div>
-                <h2>路由器信息</h2>
+            <Card
+                title={'路由器信息'}
+                extra={
+                    <Button
+                        danger
+                        icon={<LogoutOutlined />}
+                        onClick={this.logout}
+                    >
+                        断开连接
+                    </Button>
+                }
+            >
                 <h4>主机名：R{router_id}</h4>
                 <h4>IP地址：……</h4>
-            </div>
+            </Card>
         );
     }
 }
